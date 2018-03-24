ea9339e11356dae388100e1fef64468df8969386
Jeyhun Tahirov <jeyhunt@gmail.com>
2018-03-23 20:23:03 -0700

    fix typecasting issue

--- rsa/main.c
+++ rsa/main.c
@@ -21,7 +21,6 @@ static t_binput	*create_bstruct_rsa()
 	return (new);
 }
 
-
 static char		*ft_rsa_to_base64(unsigned long *res)
 {
 	t_binput	*input_base;
@@ -32,19 +31,21 @@ static char		*ft_rsa_to_base64(unsigned long *res)
 	return (answer);
 }
 
-
 int				main()
 {
-	unsigned int		*primes;
-	unsigned long		rsa_modulus;
-	unsigned long		totient;
-	unsigned int		public_constant;
-	unsigned long		inverse_modulo;
+	unsigned int			*primes;
+	unsigned long long		rsa_modulus;
+	unsigned long			totient;
+	unsigned int			public_constant;
+	unsigned long			inverse_modulo;
+	unsigned long 			temp;
 
+
+	temp = 0 - 1;
 	while(1)
 	{
 		primes = ft_get_primes();
-		rsa_modulus = primes[0] * primes[1];
+		rsa_modulus = (t_ulong)primes[0] * (t_ulong)primes[1];
 		totient = (primes[0] - 1) * (primes[1] - 1);
 		public_constant = 65537;
 		inverse_modulo = ft_get_modulo(public_constant, totient);
@@ -52,7 +53,7 @@ int				main()
 			break ;
 	}
 	ft_printf("prime1 = %lu     prime2 = %lu\n", primes[0], primes[1]);
-	ft_printf("n = %lu, phi = %lu, d = %lu\n", rsa_modulus, totient, inverse_modulo);
+	ft_printf("n = %llu, phi = %llu, d = %llu\n", rsa_modulus, totient, inverse_modulo);
 	ft_printf("e * d mod phi = %lu\n", (public_constant * inverse_modulo) % totient);
 	ft_printf("%s",ft_rsa_to_base64(&inverse_modulo));
 	return (0);
